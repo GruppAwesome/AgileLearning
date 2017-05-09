a55f644ef0e9c871f140589eace398537603340e
mikaelquick@live.se <mikaelquick@live.se>
2017-05-09 15:36:22 +0200

    changed to sha1

--- webapi/Controllers/UsersController.cs
+++ webapi/Controllers/UsersController.cs
@@ -8,33 +8,34 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace WebAPI.Controllers 
+namespace WebAPI.Controllers
 {
 
-    
+
 
     [Route("api/[controller]")]
-    public class UsersController 
+    public class UsersController
     {
         public static byte[] GetHash(string inputString)
-    {
-    HashAlgorithm algorithm = MD5.Create();  //or use SHA1.Create();
-    return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
-    }
+        {
+            HashAlgorithm algorithm = SHA1.Create();  //or use SHA1.Create();
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
 
-    public static string GetHashString(string inputString){
-        StringBuilder sb = new StringBuilder();
-        foreach (byte b in GetHash(inputString))
-        sb.Append(b.ToString("X2"));
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
 
-        return sb.ToString();
-    }
+            return sb.ToString();
+        }
 
 
         private IUserRepository userRepo;
 
-        public UsersController(IUserRepository userRepo) 
-        {   
+        public UsersController(IUserRepository userRepo)
+        {
             this.userRepo = userRepo;
         }
 
@@ -53,7 +54,7 @@ namespace WebAPI.Controllers
         }
 
 
-        public struct LoginArgs 
+        public struct LoginArgs
         {
             public string Username;
             public string Password;
@@ -64,8 +65,8 @@ namespace WebAPI.Controllers
         {
 
             var hash = GetHashString("tomat");
-            
-            Console.WriteLine(GetHash(hash));
+
+            Console.WriteLine(GetHashString("tomat"));
             var user = this.userRepo.LoginUser(args.Username, GetHashString(args.Password));
             return user;
         }
