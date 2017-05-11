ee55ab9cad940304fd43934da9bcd726001720ce
Alice Darner <alice.darner@gmail.com>
2017-05-11 10:07:08 +0200

    :do_not_litter: :exclamation:
    Made code look nicer

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,78 +1,57 @@
-(function(){
+(function () {
 
-var app = angular.module('starter', ['ionic']);
+  var app = angular.module('starter', ['ionic']);
 
-var myinfo = "lol";
-
-//Creates the controller
-app.controller('myCtrl', function($scope, $http, $state, $rootScope){
-$http.get('schooldata/data.json').success(function(data){
-  $scope.data = data;
-});
-$scope.myuser;
-$scope.info = myinfo;
-
-
- $scope.login = function() {
-        
-      
-        var username = document.getElementById("usernameInput").value;
-        var password = document.getElementById("passwordInput").value;
-
-        $http.post('http://localhost:5000/api/users/login', {Username: username,Password:password})
-        .success(function (data) {
-       
-          if(data != null && data != ""){
-         $rootScope.rootData = data;
-         $state.go('list');
-
-         
-
-        
-        }
+  //Creates the controller
+  app.controller('myCtrl', function ($scope, $http, $state, $rootScope) {
+    $http.get('schooldata/data.json').success(function (data) {
+      $scope.data = data;
     });
+    $scope.login = function () {
+      var username = document.getElementById("usernameInput").value;
+      var password = document.getElementById("passwordInput").value;
+      $http.post('http://localhost:5000/api/users/login', {
+          Username: username,
+          Password: password
+        })
+        .success(function (data) {
+          if (data != null && data != "") {
+            $rootScope.rootData = data;
+            $state.go('list');
+          }
+        });
     };
-
-
-
-
-
-
-});
-
-
-//My lovely router that redirection myviews
-app.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider.state('start', {
-    url: '/start',
-    templateUrl: 'myviews/start.html',
-    controller: 'myCtrl'
   });
 
-  $stateProvider.state('list', {
-    url: '/list',
-    templateUrl: 'myviews/list.html',
-    controller: 'myCtrl'
+
+  //My lovely router that redirection myviews
+  app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('start', {
+      url: '/start',
+      templateUrl: 'myviews/start.html',
+      controller: 'myCtrl'
+    });
+    $stateProvider.state('list', {
+      url: '/list',
+      templateUrl: 'myviews/list.html',
+      controller: 'myCtrl'
+    });
+
+    //Standard redirection
+    $urlRouterProvider.otherwise('/start');
+
   });
 
-  //Standard redirection
-  $urlRouterProvider.otherwise('/start');
-
-});
-
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   });
-});
 
 }());
