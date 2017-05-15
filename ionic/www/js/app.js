33072c7c068aa89805e946cb4f284c76f166ed87
Alice Darner <alice.darner@gmail.com>
2017-05-15 15:35:16 +0200

    Made it work with our original code
    The original navbar covered the new navbar.

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,34 +1,14 @@
-// Ionic Starter App
+(function () {
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+  var app = angular.module('starter', ['ionic']);
 
-
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.controller('myCtrl', function ($scope, $state, $ionicSideMenuDelegate, $http, $rootScope) {
-   $http.get('schooldata/data.json')
+  //Creates the controller
+  app.controller('myCtrl', function ($scope, $ionicSideMenuDelegate, $http, $state, $rootScope) {
+    $http.get('schooldata/data.json')
     .success(function (data) {
       $scope.data = data;
     });
-    /*Nytt 13.46 14/5*/
-        $scope.login = function () {
+    $scope.login = function () {
       var username = document.getElementById("usernameInput").value;
       var password = document.getElementById("passwordInput").value;
       $http.post('http://localhost:5000/api/users/login', {
@@ -42,7 +22,7 @@ angular.module('starter', ['ionic'])
           }
         });
 
-         $http.post('http://localhost:5000/api/courses/mycourses', {
+      $http.post('http://localhost:5000/api/courses/mycourses', {
           course_status: "Active",
 
         })
@@ -51,50 +31,43 @@ angular.module('starter', ['ionic'])
 
         });
 
-    }; //Login function
+    };
 
-    $scope.login2 = function () {
-      $http.post('http://localhost:5000/api/courses/mycourses', {
-          course_status: "Active"
-        })
-
-      .success(function (data) {
-            $rootScope.rootCourses = data;
-            $state.go('courses');
-
-        });
-
-    }
-
-
-
-    /*Nytt 13.46 14/5*/
-  $scope.toggleRight = function() {
+    $scope.toggleRight = function() {
     $ionicSideMenuDelegate.toggleRight()
   }
-})
- 
 
-.config(function($stateProvider, $urlRouterProvider) {
+  });
 
 
-        $stateProvider
-            .state('start', { //variabeln
-                url: '/start', //addressen finns i variabeln
-                templateUrl: 'templates/start.html', //innehållet och all material finns i t.url
-                controller: 'myCtrl'
-            })
-
-            .state('list', { //variabeln
-                url: '/list', //addressen finns i variabeln
-                templateUrl: 'templates/list.html', //innehållet och all material finns i t.url
-                controller: 'myCtrl'
-            })
-            .state('courses', { //variabeln
-                url: '/courses', //addressen finns i variabeln
-                templateUrl: 'templates/courses.html', //innehållet och all material finns i t.url
-                controller: 'myCtrl'
-            })
-
-        $urlRouterProvider.otherwise('/start'); //Vår start sida
+  //My lovely router that redirection myviews
+  app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('start', {
+      url: '/start',
+      templateUrl: 'myviews/start.html',
+      controller: 'myCtrl'
     });
+    $stateProvider.state('list', {
+      url: '/list',
+      templateUrl: 'myviews/list.html',
+      controller: 'myCtrl'
+    });
+
+    //Standard redirection
+    $urlRouterProvider.otherwise('/start');
+
+  });
+
+  app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  });
+
+}());
