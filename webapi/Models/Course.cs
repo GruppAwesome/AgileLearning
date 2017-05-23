e4ebdcbad6d724f76956d5db2b42f222588353ed
Ralle86 <ralle86@github.com>
2017-05-23 22:23:45 +0200

    Studenten ska kunna se utförlig kursplanering

--- webapi/Models/Course.cs
+++ webapi/Models/Course.cs
@@ -6,30 +6,18 @@ namespace WebAPI.Models {
 
     public class Course 
     {
-        public int course_id { get; set; }
 
         public String course_name { get; set; }
 
-        public String course_status { get; set; }
+        public String result_coursestatus { get; set; }
 
         public String course_describe { get; set; }
 
          public String course_teacher { get; set; }
 
-         public String course_exam { get; set; }
+         public String course_goal { get; set; }
+         
 
-         public String course_task { get; set; }
 
-         public int exam_id { get; set; }
-
-         public String exam_info { get; set; }
-
-         public String exam_grade { get; set; }
-
-         public int task_id { get; set; }
-
-         public String task_info { get; set; }
-
-         public String task_grade { get; set; }
     }
 }
